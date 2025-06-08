2622e7680c090511cdec989cd8cd46b9c53195a2
OmkarNarvekar001 <onarvekar01.com>
2025-06-08 12:08:04 +0530

    refresh repo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,8 @@
 package com.example;
-import java.util.logging.Logger;
 
 
 public class Main {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(getClass().getName());
-        logger.info("Hello world!");
+        System.out.println("Hello world!");
     }
 }
