c80b78d573600f0926ec5e15e3fab95eb9a4e042
OmkarNarvekar001 <onarvekar01.com>
2025-06-08 11:55:54 +0530

    refresh repo

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -8,19 +8,19 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(9, calculator.add(6, 3));
     }
 
     @Test
     public void testSubtract() {
         Calculator calculator = new Calculator();
-        assertEquals(1, calculator.subtract(3, 2));
+        assertEquals(6, calculator.subtract(8, 2));
     }
 
     @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
-        assertEquals(6, calculator.mult(3, 2));
+        assertEquals(15, calculator.mult(3, 5));
     }
 
     @Test
